f824195d04c117414f59a8d00359e4c7b21e22e2
sourav4243 <souravkumar18835@gmail.com>
2025-09-07 00:30:49 +0530

    Add Search Input, Organization Switcher and Invite Member button

--- app/dashboard/_components/Navbar.tsx
+++ app/dashboard/_components/Navbar.tsx
@@ -1,13 +1,67 @@
 "use client"
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, SignInButton, UserButton, useOrganization} from "@clerk/nextjs"
+import {SearchInput} from "./SearchInput"
+import { Unauthenticated } from "convex/react"
+import { Button } from "@/components/ui/button"
+import InviteButton from "./InviteButton"
+
 
 export const Navbar = () =>{
+    const { organization } = useOrganization()
     return(
-        <div className="flex items-center gap-x-4 p-5 bg-green-500">
-            <div className="hidden lg:flex lg:flex-1 bg-gray-400">
-                Search
+        <div className="flex items-center gap-x-4 p-5">
+            {/* Large screen */}
+            <div className="hidden lg:flex lg:flex-1 ">
+                <SearchInput/>
             </div>
-            <UserButton/>
+
+            {/* Phone */}
+            <div className="block lg:hidden flex-1">
+                <OrganizationSwitcher 
+                    hidePersonal
+                    appearance={{
+                        elements:{
+                            rootBox: {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems:"center",
+                                width: "100%",
+                                maxWidth: "376px",
+                            },
+        
+                            organizationSwitcherTrigger:{
+                                padding: "6px",
+                                width: "100%",
+                                borderRadius:"8px",
+                                border: "1px solid #E5E7EB",
+                                justifyContent:"space-between",
+                                backgroundColor: "white", 
+                            }
+                        }
+                    }}
+                />
+            </div>
+
+            {/* Invite Button */}
+            {organization && <InviteButton/> }
+
+            <Unauthenticated>
+                <SignInButton mode='modal'>
+                <Button variant={"outline"}>
+                    Log in
+                </Button>
+                </SignInButton>
+            </Unauthenticated>
+            <UserButton
+                appearance={{
+                    elements:{
+                        userButtonAvatarBox:{
+                            width: "35px",
+                            height: "35px",
+                        }
+                    }
+                }}
+            />
         </div>
     )
 }
