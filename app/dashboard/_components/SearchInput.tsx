69641715217ec996f86563eb67154200040d8f3a
sourav4243 <souravkumar18835@gmail.com>
2025-09-07 00:55:15 +0530

    Add basic search component and query-string func

---
+++ app/dashboard/_components/SearchInput.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import qs from "query-string";
+import {Search} from "lucide-react";
+import {useDebounceValue} from "usehooks-ts";
+import {useRouter} from "next/navigation";
+import{
+    ChangeEvent, 
+    useEffect, 
+    useState
+} from "react";
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = ()=>{
+    
+    const router = useRouter();
+    const [value, setValue] = useState("");
+    const [debouncedValue, setDebouncedValue] = useDebounceValue("", 500);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+        setValue(e.target.value);
+        setDebouncedValue(e.target.value);
+    }
+
+    useEffect( ()=>{
+        const url = qs.stringifyUrl({
+            url: "/dashboard",
+            query: {
+                search: debouncedValue
+            }
+        }, {skipEmptyString:true, skipNull: true});
+
+        router.push(url);
+    }, [debouncedValue, router]);
+
+    return (
+        <div className="w-full relative">
+            <Search
+                className="text-muted-foreground absolute top-1/2 left-2 transition -translate-y-1/2 border-[#e4e4ec]"
+            />
+            <Input
+                className="w-full max-w-[516px] pl-9"
+                placeholder="Search boards"
+                onChange={handleChange}
+                value={value}
+            />
+        </div>
+    )
+}
