4a15bfced3b5791eb81c9a1f8f23492fb80bc2c6
sourav4243 <souravkumar18835@gmail.com>
2025-09-07 00:25:33 +0530

    Add gradient to sidebar

--- app/dashboard/_components/sidebar/index.tsx
+++ app/dashboard/_components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import { NewButton } from "./NewButton"
 
 export const Sidebar = () =>{
     return(
-        <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4">
+        <aside className="fixed z-[1] left-0 bg-gradient-to-b from-[#8645f7] to-[#45008f] h-full w-[60px] flex flex-col p-3 gap-y-4">
             <List/>
             <NewButton/>
         </aside>
