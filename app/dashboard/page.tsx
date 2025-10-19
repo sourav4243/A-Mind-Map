11e5b113ed8154e0f67b2f5d5213d02247adf563
sourav <souravkumar18835@gmail.com>
2025-10-19 22:49:40 +0530

    search param props

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,12 +3,29 @@
 import React from "react";
 import { EmptyOrg } from "./_components/EmptyOrg";
 import { useOrganization } from "@clerk/nextjs";
+import { BoardList } from "./_components/BoardList";
 
-const page = () =>{
+interface DashboardPageProps{
+    searchParams: {
+        search?: string;
+        favorites?: string;
+    };
+};
+
+const page = ({
+    searchParams,
+} : DashboardPageProps) =>{
     const {organization} = useOrganization();
     return (
         <div className="flex-1 h-[calc(100%-80px)] p-6">
-            {!organization ? <EmptyOrg/> : (<p>Board List!</p>) }
+            {!organization ? (
+            <EmptyOrg/> 
+            ) : (
+                <BoardList
+                    ordId = {organization.id}
+                    query = {searchParams}
+                />
+            )}
         </div>
     )
 }
