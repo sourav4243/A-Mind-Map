e81b36cc96f2c2978f76ffa7f4bc5941cfd3eca4
sourav <souravkumar18835@gmail.com>
2025-10-19 12:44:59 +0530

    Empty Page for no Orgnazitaion

--- app/dashboard/_components/EmptyOrg.tsx
+++ app/dashboard/_components/EmptyOrg.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image"
+import { CreateOrganization } from "@clerk/nextjs"
+
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent, 
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog"
 
 export const EmptyOrg = () =>{
     return (
@@ -15,6 +24,19 @@ export const EmptyOrg = () =>{
             <p className="text-muted-foreground text-sm mt-2">
                 Create an Organization to get started
             </p>
+            <div className="mt-6">
+                <Dialog>
+                    <DialogTitle hidden={true}>Create Organization</DialogTitle>
+                    <DialogTrigger asChild>
+                        <Button size="lg">
+                            Create Organization
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="p-0 bg-transparent border-none max-w-[480px] w-fit">
+                        <CreateOrganization/>
+                    </DialogContent>
+                </Dialog>
+            </div>
         </div>
     )
 }
