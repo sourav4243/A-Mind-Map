70dcffda230f1914e414c59455be173feaffe525
sourav <souravkumar18835@gmail.com>
2025-10-21 00:07:37 +0530

    add new board card

--- app/dashboard/_components/BoardList.tsx
+++ app/dashboard/_components/BoardList.tsx
@@ -7,6 +7,7 @@ import { EmptySearch } from "./EmptySearch";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./NewBoardButton";
 
 interface BoardListProps {
     orgId: string;
@@ -53,6 +54,7 @@ export const BoardList = ({orgId, query} : BoardListProps) =>{
             </h2>
             
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId}/>
                 {data?.map((board) => (
                     <BoardCard
                         key={board._id}
