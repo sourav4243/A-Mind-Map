9ed6276ec2da056a322a9b927fb290c901a0ffeb
sourav <souravkumar18835@gmail.com>
2025-10-20 23:37:43 +0530

    map boards as card

--- app/dashboard/_components/BoardList.tsx
+++ app/dashboard/_components/BoardList.tsx
@@ -4,17 +4,29 @@ import { EmptyBoards } from "./EmptyBoards";
 import { EmptyFavorite } from "./EmptyFavorite";
 import { EmptySearch } from "./EmptySearch";
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
+
 interface BoardListProps {
-    ordId: string;
+    orgId: string;
     query: {
         search?: string;
         favorites?: string;
     };
 };
 
-export const BoardList = ({ordId, query} : BoardListProps) =>{
+export const BoardList = ({orgId, query} : BoardListProps) =>{
 
-    const data = []  // TODO: call API
+    const data = useQuery(api.boards.get, {orgId});
+
+    if(data === undefined){
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
 
     if(!data?.length && query.search){
         return(
@@ -36,7 +48,27 @@ export const BoardList = ({ordId, query} : BoardListProps) =>{
 
     return(
         <div>
-            {JSON.stringify(query)};
+            <h2 className="text-3xl">
+                {query.favorites? "Favorite Boards" : "Team Boards"}
+            </h2>
+            
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-5 mt-8 pb-10">
+                {data?.map((board) => (
+                    <BoardCard
+                        key={board._id}
+                        id={board._id}
+                        title={board.title}
+                        imageUrl={board.imageUrl}
+                        authorId={board.authorId}
+                        authorName={board.authorName}
+                        createdAt={board._creationTime}
+                        orgId={board.orgId}
+                        isFavorite={false}
+                    />
+                ))}
+
+            </div>
+            {/* {JSON.stringify(data)}; */}
         </div>
     );
 };
