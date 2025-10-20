f6398070459fa18a1ecc3a398a36d5fe63a18669
sourav <souravkumar18835@gmail.com>
2025-10-20 15:01:09 +0530

    use-api-mutations

--- app/dashboard/_components/EmptyBoards.tsx
+++ app/dashboard/_components/EmptyBoards.tsx
@@ -2,19 +2,19 @@
 
 import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 
 export const EmptyBoards = () => {
     const { organization } = useOrganization();
-    const create = useMutation(api.board.create);
+    const { mutate, pending } = useApiMutation(api.board.create);
 
     const addBoardThroughAPI = () =>{
         if(!organization) return;
 
-        create({
+        mutate({
             orgId: organization.id,
             title: "Untitled",
         });
@@ -35,8 +35,8 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization
             </p>
             <div className="mt-6">
-                <Button onClick={addBoardThroughAPI} size="lg">
-                    Create board
+                <Button disabled={pending} onClick={addBoardThroughAPI} size="lg">
+                    {pending ? "Creating.." : "Create Board"}
                 </Button>
             </div>
         </div>
