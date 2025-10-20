64070d8b0289e1dc900b067729e5f157d1dfd48f
sourav <souravkumar18835@gmail.com>
2025-10-20 10:21:18 +0530

    create board api endpoint

---
+++ convex/board.ts
@@ -0,0 +1,35 @@
+import { v } from "convex/values";
+
+import { mutation } from "./_generated/server";
+
+const images = [
+    "/placeholders/image1.png",
+    "/placeholders/image1.png"
+];
+
+export const create = mutation({
+    // args which are required when create a new board
+    args: {
+        ordId: v.string(),
+        title: v.string(),
+    },
+    handler: async (context, args) => {
+        const identity = await context.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error("Unauthorized");
+        }
+
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+
+        const board = await context.db.insert("boards", {
+            title: args.title,
+            orgId: args.ordId,
+            authorId: identity.subject,
+            authorName: identity.name!,     // ! is added to tell trust me bro, you think name is null but it will not be null
+            imageUrl: randomImage
+        });
+
+        return board;
+    }
+})
