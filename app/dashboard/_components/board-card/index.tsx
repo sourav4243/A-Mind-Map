287486843f9447df1a6684116643006bed136bed
sourav <souravkumar18835@gmail.com>
2025-10-21 12:49:51 +0530

    Actions for board

--- app/dashboard/_components/board-card/index.tsx
+++ app/dashboard/_components/board-card/index.tsx
@@ -6,9 +6,12 @@ import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/Actions";
+import { MoreHorizontal } from "lucide-react";
 
 import { Overlay } from "./Overlay";
 import { Footer } from "./Footer";
+import { Button } from "@/components/ui/button";
 
 interface BoardCardProps {
     id: string,
@@ -42,6 +45,17 @@ export const BoardCard = ({
                         className="object-contain"
                     />
                     <Overlay/>
+                    <Actions
+                        id={id}
+                        title={title}
+                        side="right"
+                    >
+                        <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+                            <MoreHorizontal
+                                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                            />
+                        </button>
+                    </Actions>
                 </div>
                 <Footer
                     isFavorite={isFavorite}
