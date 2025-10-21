ef394a2181053637baecbcb6f4027ad71acdc91a
sourav <souravkumar18835@gmail.com>
2025-10-21 09:42:42 +0530

    loading skeleton

--- app/dashboard/_components/BoardList.tsx
+++ app/dashboard/_components/BoardList.tsx
@@ -24,7 +24,22 @@ export const BoardList = ({orgId, query} : BoardListProps) =>{
     if(data === undefined){
         return (
             <div>
-                Loading...
+                <h2 className="text-3xl">
+                    {query.favorites? "Favorite Boards" : "Team Boards"}
+                </h2>
+                
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl-grid-cols-6 gap-5 mt-8 pb-10">
+                    {/* <NewBoardButton orgId={orgId} disabled/> */}
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                    <BoardCard.Skeleton/>
+                </div>
             </div>
         )
     }
