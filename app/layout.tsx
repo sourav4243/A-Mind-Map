35515f065bb5350ec5efc6653e79df8a9e7ba8b1
sourav <souravkumar18835@gmail.com>
2025-10-30 09:14:20 +0530

    add modalprovider for rename board

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { Toaster } from "sonner";
+import { ModalProvider } from "@/providers/modal-provider";
 
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <ConvexClientProvider>
           <Toaster/>
+          <ModalProvider/>
           {children}
         </ConvexClientProvider>
       </body>
