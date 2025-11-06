4d1f9420b9c66b26bccbf3166eb1332adc5b38e5
sourav <souravkumar18835@gmail.com>
2025-11-07 00:44:25 +0530

    fix(ui): spread query

--- app/dashboard/_components/BoardList.tsx
+++ app/dashboard/_components/BoardList.tsx
@@ -19,7 +19,10 @@ interface BoardListProps {
 
 export const BoardList = ({orgId, query} : BoardListProps) =>{
 
-    const data = useQuery(api.boards.get, {orgId, search: query.search});
+    const data = useQuery(api.boards.get, {
+        orgId,
+        ...query
+    });
 
     if(data === undefined){
         return (
