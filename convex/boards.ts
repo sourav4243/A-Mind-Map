4b686b9ecf9b90e46955d62a8616c8c309c53569
sourav <souravkumar18835@gmail.com>
2025-11-07 00:45:55 +0530

    feat(api): add support for fetching favorite boards in get boards query

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,11 +1,13 @@
 import { v } from "convex/values";
+import { getAllOrThrow } from "convex-helpers/server/relationships";
 
 import { query } from "./_generated/server";
 
 export const get = query({
     args: {
         orgId: v.string(),
-        search: v.optional(v.string())
+        search: v.optional(v.string()),
+        favorites: v.optional(v.string())
     },
     handler: async (context, args) => {
         const identity = await context.auth.getUserIdentity();
@@ -13,6 +15,29 @@ export const get = query({
         if(!identity){
             throw new Error("Unauthorized");
         }
+
+        // Favorite boards page
+        if(args.favorites){
+            const favoriteBoards = await context.db
+                .query("userFavorites")
+                .withIndex("by_user_org", (q) => 
+                    q
+                        .eq("userId", identity.subject)
+                        .eq("orgId", args.orgId)
+                )
+                .order("desc")
+                .collect();
+            
+            const ids = favoriteBoards.map((b) => b.boardId);
+
+            const boards = await getAllOrThrow(context.db, ids);
+
+            return boards.map((board) => ({
+                ...board,
+                isFavorite: true,
+            }));
+        }
+
 
         const title = args.search as string;    // search title
         let boards = [];
