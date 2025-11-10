00d64a84cd8d96e53b9249f9758c12b366201051
sourav <souravkumar18835@gmail.com>
2025-11-10 23:42:50 +0530

    feat(ui): connection Id to random color

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#DC2626",
+  "#D97706",
+  "#059669",
+  "#7C3AED",
+  "#DB2777"
+];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// accept a number and return a string
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
