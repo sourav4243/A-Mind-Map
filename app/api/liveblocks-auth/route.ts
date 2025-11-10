b2102cae7fe3462e957a58c2b8b4462134fb6b8b
sourav <souravkumar18835@gmail.com>
2025-11-10 10:06:52 +0530

    fix(api): handle null user fields

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -14,11 +14,6 @@ export async function POST(request: Request) {
     const authorization = await auth();
     const user = await currentUser();
 
-    console.log("AUTH_INFO", {
-        authorization,
-        user,
-    });
-
     if(!authorization || !user){
         return new Response("Unauthorized", { status: 403 });
     }
@@ -26,20 +21,13 @@ export async function POST(request: Request) {
     const { room } = await request.json();
     const board = await convex.query(api.board.get, {id: room});
 
-    console.log("AUTH_INFO", {
-        room,
-        board,
-        boardOrgId: board?.orgId,
-        userOrgId: authorization.orgId
-    });
-
     if(board?.orgId !== authorization.orgId){
         return new Response("Unauthorized", { status: 403 });
     }
 
     const userInfo = {
-        name: user.firstName,
-        picture: user.imageUrl,
+        name: user.firstName ?? undefined,
+        picture: user.imageUrl ?? undefined,
     };
 
     console.log({userInfo});
