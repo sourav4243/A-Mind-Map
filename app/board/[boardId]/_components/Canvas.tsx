0ba7a7b0326902af5e3b909e41d356a77dc46bd5
sourav <souravkumar18835@gmail.com>
2025-11-10 10:32:17 +0530

    chore(debug): log current user info from useSelf in Canvas

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -4,11 +4,18 @@ import { Info } from "./Info";
 import { Participants } from "./Participants";
 import { Toolbar } from "./Toolbar";
 
+import { useSelf } from "@liveblocks/react";
+
+
 interface CanvasProps {
     boardId: string;
 }
 
 export const Canvas = ({boardId} : CanvasProps) => {
+    const info = useSelf((me) => me.info);
+    
+    console.log(info);
+
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info/>
