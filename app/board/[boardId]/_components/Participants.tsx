e86571639458b80f2aba041162073d11483115a1
sourav <souravkumar18835@gmail.com>
2025-11-10 10:45:31 +0530

    refactor(ui): export ComponentSkeleton separately to keep CanvasLoading as server component

--- app/board/[boardId]/_components/Participants.tsx
+++ app/board/[boardId]/_components/Participants.tsx
@@ -6,7 +6,7 @@ export const Participants = () => {
     );
 };
 
-Participants.Skeleton = function ParticipantsSkeletoin() {
+export const ParticipantsSkeleton = () => {
     return (
         <div className="absolute h-12 w-[100px] top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md"/>
     );
