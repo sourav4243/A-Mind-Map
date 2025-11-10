e4a65670383944280493058744f37cf838fc766c
sourav <souravkumar18835@gmail.com>
2025-11-10 16:49:49 +0530

    chore(debug): remove log current user info from useSelf in Canvas

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -4,21 +4,14 @@ import { Info } from "./Info";
 import { Participants } from "./Participants";
 import { Toolbar } from "./Toolbar";
 
-import { useSelf } from "@liveblocks/react";
-
-
 interface CanvasProps {
     boardId: string;
 }
 
 export const Canvas = ({boardId} : CanvasProps) => {
-    const info = useSelf((me) => me.info);
-    
-    console.log(info);
-
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
-            <Info/>
+            <Info boardId={boardId}/>
             <Participants/>
             <Toolbar/>
         </main>      
