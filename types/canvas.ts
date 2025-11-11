b7ced8de64d26c5e7616b938c3698dc258f76096
sourav <souravkumar18835@gmail.com>
2025-11-11 12:08:26 +0530

    feat(types): add CanvasState union type and CanvasMode enum for canvas interaction states

---
+++ types/canvas.ts
@@ -0,0 +1,32 @@
+export type CanvasState = 
+    |   {
+        mode: CanvasMode.None;
+        }
+    |   {
+        mode: CanvasMode.Pressing
+        }
+    |   {
+        mode: CanvasMode.SelectionNet
+        }
+    |   {
+        mode: CanvasMode.Translating
+        }
+    |   {
+        mode: CanvasMode.Inserting
+        }
+    |   {
+        mode: CanvasMode.Resizing
+        }
+    |   {
+        mode: CanvasMode.Pencil
+    };
+
+export enum CanvasMode {
+    None,
+    Pressing,
+    SelectionNet,
+    Translating,
+    Inserting,
+    Resizing,
+    Pencil,
+};
