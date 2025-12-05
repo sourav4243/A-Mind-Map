72b64885e6e735b38c98d44ae1b53241f34068dc
sourav <souravkumar18835@gmail.com>
2025-12-06 00:00:24 +0530

    feat(layer): LayerPreview component with type-based rendering

---
+++ app/board/[boardId]/_components/LayerPreview.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { LayerType } from "@/types/canvas";
+import { useStorage } from "@liveblocks/react";
+import { memo } from "react";
+import { Rectangle } from "./Rectangle";
+
+interface LayerPreviewProps {
+    id: string;
+    onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
+    selectionColor?: string;
+};
+
+export const LayerPreview = memo(({
+    id,
+    onLayerPointerDown, 
+    selectionColor,
+}:LayerPreviewProps) => {
+
+    const layer = useStorage((root) => root.layers.get(id));
+
+    if(!layer) {
+        return null;
+    }
+
+    // Render based on type of layer
+    switch (layer.type) {
+        case LayerType.Rectangle:
+            return(
+                <Rectangle
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}                   
+                />
+            )
+        default:
+            console.warn("Unknown layer type");
+            return null;
+    }
+});
+
+LayerPreview.displayName = "LayerPreview";
