248c7b28e3d5c55872c9ff7ef99250ee49cc6cba
sourav <souravkumar18835@gmail.com>
2025-12-09 01:23:26 +0530

    feat(cursor): add realtime rendering of remote pencil drafts

--- app/board/[boardId]/_components/CursorsPresense.tsx
+++ app/board/[boardId]/_components/CursorsPresense.tsx
@@ -2,8 +2,10 @@
 
 import { memo } from "react";
 
-import { useOthersConnectionIds } from "@liveblocks/react";
+import { shallow, useOthersConnectionIds, useOthersMapped } from "@liveblocks/react";
 import { Cursor } from "./Cursor";
+import { Path } from "./Path";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
     const ids = useOthersConnectionIds();
@@ -20,10 +22,35 @@ const Cursors = () => {
     );
 };
 
+const Drafts = () => {
+    const others = useOthersMapped((other) => ({
+        pencilDraft: other.presence.pencilDraft,
+        penColor: other.presence.penColor,
+    }), shallow);
+
+    return (
+        <>
+            {others.map(([key, other]) => {
+                if (other.pencilDraft) {
+                    return (
+                        <Path
+                            key={key}
+                            x={0}
+                            y={0}
+                            points={other.pencilDraft}
+                            fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+                        />
+                    );
+                }
+            })}
+        </>
+    );
+};
+
 export const CursorPresense = memo(() => {
     return (
         <>
-            {/* TODO: Draft Pencil */}
+            <Drafts/>
             <Cursors/>
         </>
     );
