c4e787f5607a113240ee1ce40afb8e9de0fe12de
sourav <souravkumar18835@gmail.com>
2025-12-08 16:14:33 +0530

    feat(utils): add helper to find layers intersecting a drag-selection rectangle

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,8 @@ import React from "react";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-import { Camera, Color, XYWH, Side, Point } from "@/types/canvas";
+import { Camera, Color, XYWH, Side, Point, Layer } from "@/types/canvas";
+import { v } from "convex/values";
 
 const COLORS = [
   "#DC2626",
@@ -65,4 +66,44 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
   }
 
   return result;
+}
+
+export function findIntersectingLayersWithRectangle(
+  layerIds: readonly string[] | null,
+  layers: ReadonlyMap<string, Layer>,
+  a: Point, 
+  b: Point,
+) {
+  const rect = {
+    x: Math.min(a.x, b.x),
+    y: Math.min(a.y, b.y),
+    width: Math.abs(a.x - b.x),
+    height: Math.abs(a.y - b.y),
+  };
+
+  const ids = [];
+  
+  if (!layerIds) {
+    return;
+  }
+
+  for (const layerId of layerIds) {
+    const layer = layers.get(layerId);
+
+    if (layer == null) {
+      continue;
+    }
+
+    const { x, y, height, width } = layer;
+
+    if (
+      rect.x + rect.width > x   &&
+      rect.x < x + width        &&
+      rect.y + rect.height > y   &&
+      rect.y < y + height
+    ) {
+      ids.push(layerId);
+    }
+  }
+  return ids;
 }
