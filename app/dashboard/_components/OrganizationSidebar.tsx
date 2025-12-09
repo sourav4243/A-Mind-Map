f842d44f1d9b495b1529bfcf03c8cebbac3af43e
sourav <souravkumar18835@gmail.com>
2025-12-09 17:38:34 +0530

    chore(OrganisationSideBar): remove Poppin font

--- app/dashboard/_components/OrganizationSidebar.tsx
+++ app/dashboard/_components/OrganizationSidebar.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import { Poppins } from "next/font/google";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, Star } from "lucide-react";
